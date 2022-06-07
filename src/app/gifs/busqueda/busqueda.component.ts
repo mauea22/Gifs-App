301b10f85e475558f2a836dd2810c371d1762cd8
mauea22 <mauea22@hotmail.com>
2022-06-07 14:35:46 -0300

    @ViewChild para obtener referencias

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent  {
+//el @ViewChild busca una referencia en el html (en este caso el #txtBuscar) y lo asigna a txtBuscar de tipo any
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  buscar( termino:string){
+    //asigno el valor a una variable
+    const valor = this.txtBuscar.nativeElement.value;
 
-  ngOnInit(): void {
+    console.log(this.txtBuscar);
+    //limpio el formulario 
+    this.txtBuscar.nativeElement.value = "";
   }
-
 }
